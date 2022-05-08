9c5be3b755cc3712b071b31010ac2c33c883949d
Akira Taguchi <akirahattivatti@gmail.com>
2022-05-08 12:57:41 +0300

    Convert to React frontend

--- src/main/java/com/solidabis/koodihaaste22/restaurants/dtos/RestaurantResponseDTO.java
+++ src/main/java/com/solidabis/koodihaaste22/restaurants/dtos/RestaurantResponseDTO.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class RestaurantResponseDTO {
     @Schema(description = "If non-null, contains the voted restaurant id for today")
     private String alreadyVoted;
+    @Schema(description = "VoterID cookie")
+    private String voterId;
     @Schema(description = "Current date")
     private String date;
     @Schema(description = "List of restaurants")
--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,99 +1,58 @@
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
 
 function App() {
-  let cookie
+  const [restaurants, setRestaurants] = useState([])
 
-  async function getapi(url) {
-    const response = await fetch(url);
-    var data = await response.json();
-    if (response) {
-      hideloader();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:8080/api/v1/restaurants/helsinki', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      const json = await data.json()
+      Cookies.set('VOTERID', json.voterId)
+      setRestaurants(json.restaurants)
+      return json
     }
-    console.log(data.voterId)
-    cookie = data.voterId
-    show(data);
-  }
 
-  // Ravintola data rajapinnasta.
-  const api_url =
-    "http://localhost:8080/api/v1/restaurants/helsinki";
-  getapi(api_url);
+    fetchData()
+  }, [])
 
-  // Äänestys.
-  function vote(id) {
-    console.log(document.cookie)
-    console.log(id)
+  const vote = (id) => {
     let url = `http://localhost:8080/api/v1/vote/${id}`;
-    let data = { restaurantid: id };
 
     fetch(url, {
-      credentials: "same-origin",
       method: "POST",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
+      headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/json' },
+      credentials: "same-origin",
     }).then(res => {
       console.log("Request complete! response:", res);
     });
   }
 
-  function hideloader() {
-    document.getElementById('loading').style.display = 'none';
-  }
-
-  function show(data) {
-    let restaurantList = data["restaurants"];
-    let voted = false;
-    if (data["alreadyVoted"] != null) { voted = true };
-    let vote = "";
-
-    let tab =
-      ``;
-    // Ravintola- moduulit datasta.
-    for (let restaurants of restaurantList) {
-      let nimi = restaurants.name;
-      let auki = restaurants.openingHours; if (auki === "") { auki = "-" };
-      let äänet = restaurants.votes
-      if (!voted) { vote += `<button onclick="vote('${restaurants.id}')"> +1! </button> ` }
-      tab += `
-                <h3><strong> ${nimi}</strong></h3>
-                <ul>
-                    <li>
-                        <strong>Aukiolo:</strong> ${auki}
-                    </li>
-                    <li>
-                    <strong>Äänet:</strong> ${äänet}
-                    </li>
-                    ${vote}
-                </ul>`; vote = "";
-
-    }
-
-    document.getElementById("restaurants").innerHTML = tab;
-  }
-
   return (
     <div>
-      <h1>
-        Helsingin Tikkataulu
-      </h1>
-      <div>
-        <div className="d-flex justify-content-center">
-          <div className="spinner-border"
-            role="status" id="loading">
-            <span className="sr-only">loading...</span>
+      <h1>Helsingin Tikkataulu</h1>
+      <h3>Restaurangs;</h3>
+      <div className="restaurants" id="restaurants">
+        {restaurants.map(restaurant =>
+          <div>
+            <h3><strong>{restaurant.name}</strong></h3>
+            <ul>
+              <li key={restaurant.id}><strong>Aukiolo:</strong> {restaurant.openingHours}</li>
+              <li key={restaurant.name}><strong>Äänet:</strong> {restaurant.votes}</li>
+            </ul>
+            <button onClick={() => vote(restaurant.id)}>+1!</button>
           </div>
-        </div>
-        <h3>Restaurangs;</h3>
-
-        <div className="container justify-content-center border" id="restaurants">
-          <div className="row justify-content-center border" id="restaurants">
-            <div className="col justify-content-center border" id="restaurants">
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
-  );
-
+  )
 }
 export default App;
